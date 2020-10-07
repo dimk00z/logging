4d0a46f8a26901375992a0df771a01e9b84208a7
Dmitry <dimk00z@gmail.com>
2020-10-07 11:54:44 +0300

    lesson 6

--- main.py
+++ main.py
@@ -6,8 +6,23 @@ logging.config.dictConfig(logger_config)
 app_logger = logging.getLogger('app_logger')
 
 
+def print_into_debug():
+
+    app_logger.debug('Debug from print_into_debug()',
+                     extra={'some': 'thing'})
+
+
 def main():
-    app_logger.debug(f'Enter in to the main()')
+    print_into_debug()
+
+    app_logger.debug('Debug from main()', extra={'some': 'thing'})
+    words = ['new house', 'apple', 'ice cream', 3]
+    for item in words:
+        try:
+            1/0
+            print(item.split(' '))
+        except:
+            app_logger.exception('Some', extra={'some': 'thing'})
 
 
 if __name__ == "__main__":
--- setting.py
+++ setting.py
@@ -1,4 +1,24 @@
-format_string = '{asctime} - {levelname} - {name} - {message}'
+import logging
+
+
+class MyFilter(logging.Filter):
+    def filter(self, record):
+        print(record.some)
+        return record.funcName == 'print_into_debug'
+
+
+class My_Handler(logging.Handler):
+    def __init__(self, file_name):
+        logging.Handler.__init__(self)
+        self.file_name = file_name
+
+    def emit(self, record):
+        message = self.format(record)
+        with open(self.file_name, 'a') as file:
+            file.write(message+'\n')
+
+
+format_string = '{asctime} - {levelname} - {name} - {module}:{funcName}:{lineno}- {message}'
 logger_config = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -12,7 +32,8 @@ logger_config = {
         'console_handler': {
             'class': 'logging.StreamHandler',
             'level': 'DEBUG',
-            'formatter': 'std_formatter'
+            'formatter': 'std_formatter',
+            'filters': ['my_filter']
         },
         'file_handler': {
             'class': 'logging.FileHandler',
@@ -20,15 +41,25 @@ logger_config = {
             'formatter': 'std_formatter',
             'filename': 'debug.log'
         },
+        'my_file_handler': {
+            '()': My_Handler,
+            'level': 'DEBUG',
+            'formatter': 'std_formatter',
+            'file_name': 'my_debug.log'
+        },
     },
     'loggers': {
         'app_logger': {
             'level': 'DEBUG',
-            'handlers': ['console_handler', 'file_handler'],
+            'handlers': ['console_handler', 'file_handler', 'my_file_handler'],
             # 'propagate':False
         }
     },
-    # 'filters': {},
+    'filters': {
+        'my_filter': {
+            '()': MyFilter  # экземпляр класса
+        }
+    },
     # 'root': {}  # '':{}
     # 'incremental':True
 }
